9967af1856922d1c7203e109c0447d573b44bace
Yousuf <yrajput@ucsd.edu>
2019-05-01 10:17:20 -0700

    initial init

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,24 @@
+package edu.ucsd.cs110.temperature;
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float value = ((this.getValue()-32)*5/9);
+        return new Fahrenheit(value);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return this;
+    }
+
+    public String toString()
+    {
+        return this.getValue()+" F";
+    }
+}
